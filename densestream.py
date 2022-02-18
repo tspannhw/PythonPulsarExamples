f70ac1e2208299055d2fe50f540775df57889982
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-02-18 16:22:14 -0500

    Create densestream.py

---
+++ densestream.py
@@ -0,0 +1,39 @@
+# https://github.com/streamnative/developing_pulsar_exercise/blob/master/3_Streaming/solutions/java/src/main/java/sn_training/ex4_reader/DenseStream.java
+# https://docs.google.com/spreadsheets/d/1YHYTkIXR8-Ql103u-IMI18TXLlGStK8uJjDsOOA0T20/edit#gid=1031448742
+# https://pulsar.apache.org/api/python/#pulsar.Client
+import time
+import pulsar
+import sys
+import datetime
+import re
+from time import gmtime, strftime
+import random, string
+from pulsar.schema import *
+from time import sleep
+import traceback
+import random
+from pulsar import ConsumerType, InitialPosition, BatchingType,PartitionsRoutingMode
+
+def callback(res, msg_id):
+    print("Message published: {} {}".format(str(res), str(msg_id)))
+
+msgNum = 0
+currentMessage = ""
+currentProducer = "DenseStream"
+topic = "stream-sample"
+
+client = pulsar.Client('pulsar://localhost:6650')
+producer = client.create_producer(topic=topic, schema=schema.StringSchema(), send_timeout_millis=10000,block_if_queue_full=True,properties={"producer-name": currentProducer,"producer-id": currentProducer})
+
+while True:
+    try:
+        msgNum = msgNum + 1
+        currentMessage = str(msgNum)
+        print("Sending message [{}] at [{}]".format(str(currentMessage), str(datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S'))))
+        producer.send_async(currentMessage, callback=callback)
+
+        time.sleep(1)
+
+    except Exception as e: print(e)
+
+client.close()
